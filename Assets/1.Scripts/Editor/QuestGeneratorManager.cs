2028599680e932bf82f2545c4b7f2b6dd538252f
Hyoung-Seok <dkavmdk98@gmail.com>
2024-09-25 21:49:38 +0900

    Fix : 파일 이름 검사 내용 추가

--- Assets/1.Scripts/Editor/QuestGeneratorManager.cs
+++ Assets/1.Scripts/Editor/QuestGeneratorManager.cs
@@ -9,12 +9,14 @@ public class QuestGeneratorManager : EditorWindow
 {
     [Header("Data")] 
     public static string NpcData;
+    public static string OtherData;
     
     [MenuItem("OpenAI/QuestGenerator")]
     private static void CreateWindow()
     {
         ResultCustomWindow.ShowResultWindow();
         NpcDataUI.CreateWindow();
+        OtherDataUI.CreateWindow();
     }
 
     public void CreateGUI()
@@ -25,7 +27,13 @@ public class QuestGeneratorManager : EditorWindow
     public static string GetFullFilePath(string path, string name)
     {
         var result = Path.Combine(Application.dataPath, path);
-        result = Path.Combine(result, name + ".xlsx");
+
+        if (name.Contains(".xlsx") == false)
+        {
+            name += ".xlsx";
+        }
+        
+        result = Path.Combine(result, name);
 
         return result;
     }
