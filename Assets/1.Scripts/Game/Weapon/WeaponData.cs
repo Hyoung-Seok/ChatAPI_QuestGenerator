69fa8e448c982a4da2da98e1673352a03abc5bdd
Hyoung-Seok <dkavmdk98@gmail.com>
2024-10-08 01:51:51 +0900

    Feat : 무기 정보 스크립터블 생성

---
+++ Assets/1.Scripts/Game/Weapon/WeaponData.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "WeaponData", menuName = "Scriptable Object/WeaponData", order = int.MaxValue)]
+public class WeaponData : ScriptableObject
+{
+    [Header("Transform - Back")] 
+    [SerializeField] private Vector3 onBackPosition;
+    [SerializeField] private Vector3 onBackRotation;
+    [SerializeField] private Vector3 onBackScale;
+    
+    [Header("Transform - Hand")]
+    [SerializeField] private Vector3 onHandPosition;
+    [SerializeField] private Vector3 onHandRotation;
+    [SerializeField] private Vector3 onHandScale;
+
+    [Header("Object")] 
+    [SerializeField] private GameObject weaponModel;
+
+    [Header("State")] 
+    [SerializeField] private float damage;
+    [SerializeField] private float fireRate;
+    [SerializeField] private int magazine;
+
+    public Vector3 OnBackPosition => onBackPosition;
+    public Vector3 OnBackRotation => onBackRotation;
+    public Vector3 OnBackScale => onBackScale;
+
+    public Vector3 OnHandPosition => onHandPosition;
+    public Vector3 OnHandRotation => onHandRotation;
+    public Vector3 OnHandScale => onHandScale;
+
+    public GameObject WeaponModel => weaponModel;
+
+    public float Damage => damage;
+    public float FireRate => fireRate;
+    public int Magazine => magazine;
+}
