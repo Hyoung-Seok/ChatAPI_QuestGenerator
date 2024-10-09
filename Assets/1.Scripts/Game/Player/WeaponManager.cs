394ed68449028fc4af889c5f91e31f3699679e6d
Hyoung-Seok <dkavmdk98@gmail.com>
2024-10-10 03:08:42 +0900

    Fix : AnimationEvent와 RigController 기능 병합

---
+++ Assets/1.Scripts/Game/Player/WeaponManager.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations.Rigging;
+using UnityEngine.Serialization;
+
+public enum ERigState
+{
+    BACK,
+    IDLE,
+    AIM
+}
+
+public class RigController : MonoBehaviour
+{
+    
+}
