c670b64b2e069f123dd0f1b1de4be0d90c34341c
Hyoung-Seok <dkavmdk98@gmail.com>
2024-10-25 19:21:10 +0900

    Feat : 현재 퀘스트 관리를 위한 매니저 클래스 생성

---
+++ Assets/1.Scripts/Game/GameSystem/QuestManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EQuestState
+{
+    Start,
+    Processing,
+    Completion
+}
+public class QuestManager : MonoBehaviour
+{
+    
+}
